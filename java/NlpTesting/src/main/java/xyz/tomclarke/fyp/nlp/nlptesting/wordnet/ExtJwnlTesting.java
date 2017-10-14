754977f30c3b44ba8dd6f1bc16d56010f9790180
Tom Clarke <tomclarke96@hotmail.co.uk>
2017-10-14 14:06:44 +0100

    Tested out extJWNL and found it to be the best tool for WordNet so far
    - Refactored packages to follow Java standard
    - Added extJWNL package
    - Simple tests and configuration show extJWNL to be the most useful/flexible so far.

---
+++ java/NlpTesting/src/main/java/xyz/tomclarke/fyp/nlp/nlptesting/wordnet/ExtJwnlTesting.java
@@ -0,0 +1,77 @@
+package xyz.tomclarke.fyp.nlp.nlptesting.wordnet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.extjwnl.JWNLException;
+import net.sf.extjwnl.data.IndexWord;
+import net.sf.extjwnl.data.POS;
+import net.sf.extjwnl.data.Synset;
+import net.sf.extjwnl.data.Word;
+import net.sf.extjwnl.dictionary.Dictionary;
+
+/**
+ * Testing the extJWNL package - overall it seems easy to setup and to use, so
+ * will probably be the way I access WordNet in code from now onwards.
+ * 
+ * @author tbc452
+ *
+ */
+public class ExtJwnlTesting {
+
+    public static void main(String[] args) throws JWNLException {
+        new ExtJwnlTesting();
+    }
+
+    public ExtJwnlTesting() throws JWNLException {
+        Dictionary d = Dictionary.getDefaultResourceInstance();
+
+        // Noun - Bed
+        IndexWord word = d.getIndexWord(POS.NOUN, "bed");
+        printPossibleSynonyms("bed", getPossibleSynonyms(word.getSenses()));
+
+        // Noun - facial recognition
+        word = d.getIndexWord(POS.NOUN, "facial recognition");
+        printPossibleSynonyms("facial recognition", getPossibleSynonyms(word.getSenses()));
+
+    }
+
+    /**
+     * Creates lists of possible synonyms
+     * 
+     * @param synsets
+     *            The result of looking up a word
+     * @return A list of possible sets of synonyms
+     */
+    public List<List<String>> getPossibleSynonyms(List<Synset> synsets) {
+        List<List<String>> sets = new ArrayList<List<String>>();
+
+        for (Synset synset : synsets) {
+            List<String> synonyms = new ArrayList<String>();
+            for (Word word : synset.getWords()) {
+                synonyms.add(word.getLemma());
+            }
+            if (!synonyms.isEmpty()) {
+                sets.add(synonyms);
+            }
+        }
+
+        return sets;
+    }
+
+    /**
+     * Prints out synonyms
+     * 
+     * @param word
+     *            The original word
+     * @param possibleSynonyms
+     *            The sets of synonyms
+     */
+    public void printPossibleSynonyms(String word, List<List<String>> possibleSynonyms) {
+        System.out.println("Original word: " + word);
+        for (List<String> synonyms : possibleSynonyms) {
+            System.out.println("Possible synonyms: " + synonyms);
+        }
+    }
+
+}
