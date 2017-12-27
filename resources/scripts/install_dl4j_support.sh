cc02788f110cd735325037090cfa7b400c128551
tbc452 <tbc452@student.bham.ac.uk>
2017-12-27 11:11:53 +0000

    Removed redundant script

--- resources/scripts/install_dl4j_support.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Install dependencies of ND4J / DL4J / Word2Vec (investigate further)
-echo -n 'Running `apt-get install gfortran libopenblas-dev liblapack-dev`... '
-sudo apt-get install -qq gfortran libopenblas-dev liblapack-dev
-echo 'done'
