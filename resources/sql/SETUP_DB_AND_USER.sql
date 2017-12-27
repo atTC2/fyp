3053a9a7849b04ea86f4eff973450d0a53ae09c7
tbc452 <tbc452@student.bham.ac.uk>
2017-12-27 17:38:33 +0000

    Generate the build SQL script

--- resources/sql/SETUP_DB_AND_USER.sql
+++ resources/sql/SETUP_DB_AND_USER.sql
@@ -1,6 +1,7 @@
 -- Create the FYP database
-CREATE DATABASE fyp;
+DROP SCHEMA IF EXISTS `fyp` ;
+CREATE SCHEMA IF NOT EXISTS `fyp` DEFAULT CHARACTER SET utf8 ;
 
 -- Create the FYP user and give permissions
-CREATE USER 'fyp_user'@'localhost' IDENTIFIED BY 'SPaZdZcjyNwpjMAgpBfExQNvDdx6p4cRmW4ZzuUPHF73eHhCKtXRsqCrQsAdkytN';
-GRANT ALL ON fyp . * TO 'fyp_user'@'localhost';
+CREATE USER 'fyp_user'@'%' IDENTIFIED BY 'SPaZdZcjyNwpjMAgpBfExQNvDdx6p4cRmW4ZzuUPHF73eHhCKtXRsqCrQsAdkytN';
+GRANT ALL ON fyp . * TO 'fyp_user'@'%';
---
+++ resources/sql/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run the script to build the database
+mysql -u fyp_user -pSPaZdZcjyNwpjMAgpBfExQNvDdx6p4cRmW4ZzuUPHF73eHhCKtXRsqCrQsAdkytN < BUILD_FYP_DB.sql
+
