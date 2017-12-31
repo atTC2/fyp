c9fd599d99beb57ee017fdd872187bc27f2dd5d3
tbc452 <tbc452@student.bham.ac.uk>
2017-12-31 10:21:12 +0000

    Changed  to

--- java/FYP-NLP/update-xyz.sh
+++ java/FYP-NLP/update-xyz.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Update tom@tomclarke.xyz (by-pass Git for testing purposes)
-scp -P 8022 -r src/ tom@tomclarke.xyz:/home/tom/FYP/tbc452/java/FYP/
+scp -P 8022 -r src/ tom@tomclarke.xyz:/home/tom/FYP/tbc452/java/FYP-NLP/
 
